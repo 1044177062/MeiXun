0c4f8babc28a3ef23e6d21b51bfb92c5612eac61
Awaion <1044177062@qq.com>
2019-07-19 16:50:58 +0800

    2019/7/19日更新

--- js/js.js
+++ js/js.js
@@ -1,4 +1,29 @@
+var i=0;
+var sl=0;
+function lunbo(){
+	i=i+1;
+	if(i>=5){i=1}
+	$(".lunboul li").hide();
+	$(".lunbo_"+i).show();
+	$(".lunbotext li").attr("class","xz_no");
+	$(".lunbotext li:nth-child("+i+")").attr("class","xz_yes");
+}
+
+setInterval("lunbo()",2000);
+
 $(function(){
+	$(".lunbotext li").each(function(s){
+		s=s+1;
+		$(this).click(function(){
+			i=s;
+			$(".lunboul li").hide();
+			$(".lunbo_"+i).show();
+			$(".lunbotext li").attr("class","xz_no");
+			$(".lunbotext li:nth-child("+i+")").attr("class","xz_yes");
+		})
+	})
+	
+	
 	$(".hed_r_bg").mouseover(function(){
 		$(this).children(".hed_r_1").show();
 	}).mouseout(function(){
@@ -15,5 +40,19 @@ $(function(){
 		$(this).children(".wdgwc").show();
 	}).mouseout(function(){
 		$(this).children(".wdgwc").hide();
+	})
+	
+	$(".left").each(function(i){
+		i=i+1;
+		$(this).mouseover(function(){
+			$(".dwzs").hide();
+			$(".zs"+i).show();
+		}).mouseout(function(){
+			$(".dwzs").hide();
+		})
+	})
+	
+	$(".syspbut").click(function(){
+		$(".body_left").toggle();
 	})
 })
---
+++ js/lodjs.js
@@ -0,0 +1,31 @@
+$(function(){
+	$("#yhm").blur(xzuser);
+	$("#mm").blur(xzpwd);
+	
+	$("#myform").submit(function(){
+		var flag=true;
+		if(!xzuser()) flag=false;
+		if(!xzpwd()) flag=false;
+		return flag;
+	})
+})
+
+function xzuser(){
+	var yhm=$("#yhm").val();
+	if(yhm==""){
+		$(".yhmid").html("输入不能为空");
+		return false
+	}
+	$(".yhmid").html(".");
+	return true
+}
+
+function xzpwd(){
+	var mm=$("#mm").val();
+	if(mm==""){
+		$(".mmid").html("输入不能为空");
+		return false
+	}
+	$(".mmid").html(".");
+	return true
+}
